69f5cf4ac5deff13ce94d9f23539bfed4f8cbb45
Vijetha Narendrapurapu <nvijetha@gmail.com>
2013-04-16 18:04:47 +0530

    changes in Gruntfile.js

--- Gruntfile.js
+++ Gruntfile.js
@@ -30,5 +30,5 @@ module.exports = function(grunt){
 	grunt.loadNpmTasks('grunt-contrib-jshint');
 	grunt.loadNpmTasks('grunt-contrib-concat');
 	grunt.loadNpmTasks('grunt-contrib-jasmine');
-	grunt.registerTask('default',['jshint']);
+	grunt.registerTask('default',['jshint', 'concat', 'uglify', 'jasmine']);
 };
